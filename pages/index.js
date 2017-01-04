df5c7f1d0ded539b696bb15553b3eb232955dcc0
Julien Bouquillon <julien@revolunet.com>
2017-01-04 16:54:09 +0100

    next.js: prevent style flickr, inject css in page head

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Head from 'next/head'
-
+import styleSheet from 'styled-components/lib/models/StyleSheet'
+import 'isomorphic-fetch'
 import LinksBrowser from '../components/LinksBrowser'
 
 
-const Page = ({ links }) => (
+
+
+const Page = ({ style, links }) => (
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>React Workshop #1</title>
       <link rel="stylesheet" href="/static/fade.css"/>
+      <style dangerouslySetInnerHTML={{ __html: style }} />
     </Head>
     <LinksBrowser links={ links }/>
   </div>
@@ -20,9 +24,10 @@ const Page = ({ links }) => (
 // NB: in next.js only top-level components can have getInitialProps
 class PageContainer extends React.Component {
   static async getInitialProps() {
+    const style = styleSheet.rules().map(rule => rule.cssText).join('\n')
     const res = await fetch('http://127.0.0.1:3000/static/links.json')
     const links = await res.json()
-    return { links }
+    return { links, style }
   }
   render() {
     return <Page { ...this.props }/>
