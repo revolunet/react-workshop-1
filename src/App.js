df261218f0df6dd1acb4c06ddc05409ca0bea9b3
Julien Bouquillon <julien@revolunet.com>
2017-01-05 23:58:40 +0100

    step 2 : basic component
     - Import [data](https://raw.githubusercontent.com/revolunet/react-workshop-1/c4f6971cfd36a30db5a911712a0790e1699ae097/static/links.json) into src/links.json
     - Create a basic Links component
     - Use the console and add a key perf hint

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import Links from './Links'
+import links from './links.json'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <Links links={links} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
